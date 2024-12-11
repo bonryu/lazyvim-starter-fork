20aa8473cb195184479697947c263059cf505d9e
Bon Ryu <bonryu@gmail.com>
2024-12-11 16:42:09 -0500

    markdown highlighting

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,17 @@
+local HOME = os.getenv("HOME")
+return {
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        markdown = { "markdownlint-cli2" },
+      },
+      linters = {
+        ["markdownlint-cli2"] = {
+          args = { "--config", HOME .. "/.markdownlint-cli2.yaml", "--" },
+        },
+      },
+    },
+  },
+}
